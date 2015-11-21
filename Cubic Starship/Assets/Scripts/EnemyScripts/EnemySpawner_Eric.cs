82a9945333d982b9a0e14831f3862b7562bfc520
MilesGloriosus <Tripitaca83@gmail.com>
2015-11-21 15:47:21 -0500

    Changed spawning structure
    Spawning structure is SpawnNode -> EnemySpawner_Eric, where spawn node
    is just the info, and EnemySpawner is the timing between each nodes.

---
+++ Cubic Starship/Assets/Scripts/EnemyScripts/EnemySpawner_Eric.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum SpawnerState
+{
+	SpawnerIdle,
+	SpawnerSpawning
+}
+
+
+public class EnemySpawner_Eric : MonoBehaviour {
+	public SpawnNode spawn;
+
+	private bool m_bSpawning;
+	private SpawnerState m_State;
+
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void TriggerSpawn()
+	{
+		m_State = SpawnerState.SpawnerSpawning;
+
+		//start spawning all 
+	}
+}
---
+++ Cubic Starship/Assets/Scripts/EnemyScripts/SpawnNode.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SpawnTiming
+{
+	AtStartOfNodeReference,
+	AtEndOfNodeReference
+};
+
+[System.Serializable]
+public class SpawnNode
+{
+	public GameObject spawn;		//The enemy to spawn
+	public GameObject spawnPath;    //The path each enemy will follow when spawned.
+									//10-21-15 Eric: For right now, they will only follow a BezierSpline script. The BezierSpline script must 
+									// be attached to the game object. If there is no BezierSpline script, the enemy will spawn at the transform's
+									// origin only and sit there.
+
+	public uint spawnAmount;		//Total amount of enemies spawned
+	public float spawnRate;         //Time between each enemy spawned
+//	public float timeOffset;        //The time to wait before spawning the first enemy
+	public GameObject spawnParent;  //The parent to attach the enemies too.
+//	public SpawnTiming nodeSpawnTiming;
+//	public SpawnNode nodeSpawnTimingReference;
+}
